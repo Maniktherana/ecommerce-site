912d8b8922eb045e1daab9065b45dac35b88a5cd
Manik <manikrana54@gmail.com>
2022-08-06 17:38:49 +0530

    Complete layout

--- next-app/components/Layout.jsx
+++ next-app/components/Layout.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { Children } from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>The Online Tech Store</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
